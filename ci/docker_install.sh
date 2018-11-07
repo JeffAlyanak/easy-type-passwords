8da4c0568c88f6e96eaa0fa2485139443ea0f788
rights.ninja <admin@rights.ninja>
2018-11-07 15:56:17 -0500

    Added initial ci config.

---
+++ ci/docker_install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+[[ ! -e /.dockerenv ]] && exit 0
+
+set -xe
+
+apt-get update -yqq
+apt-get install git -yqq
+
+curl --location --output /usr/local/bin/phpunit https://phar.phpunit.de/phpunit.phar
+chmod +x /usr/local/bin/phpunit
