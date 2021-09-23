155a64d65a7462ef7cfe401d805fbfd0ec4c89e7
Jeff Alyanak <jeff@alyanak>
2021-09-23 12:51:44 +0100

    Added special characters
    These work very much like the numbers and fill a random word edge.

--- src/assets/php/class/password-generator.php
+++ src/assets/php/class/password-generator.php
@@ -4,14 +4,19 @@ require_once	"wordlist-parser.php";
 
 final class PasswordGenerator
 {
-	public function generatePassword($howManyWords, $howManyNumbers): string
-	{		
+	public function generatePassword($howManyWords, $howManyNumbers, $specialCharacters): string
+	{
 		$password	= '';
 
 		$parser	= new WordlistParser;
-		
-		$edges	= $howManyWords + 1;								// Edges between words, ie, places to put numbers.
-		if ($howManyNumbers > $edges) $howManyNumbers = $edges;		// Limit numbers based on places to put them.
+
+		// Shuffle the special characters and get the length
+		$specialCharacters = str_shuffle($specialCharacters);
+		$howManySpecialCharacters = strlen($specialCharacters);
+
+		$edges	= $howManyWords + 1;												// Edges between words, ie, places to put numbers.
+		if ($howManyNumbers > $edges) $howManyNumbers = $edges;						// Limit numbers based on places to put them.
+		if ($howManySpecialCharacters > $edges) $howManySpecialCharacters = $edges;	// Limit special chars based on places to put them.
 
 		$a	= $parser->ListLength();
 		if ($a != 0)
@@ -27,6 +32,17 @@ final class PasswordGenerator
 
 				shuffle($locationOfNumbers);
 			}
+			// Choose unique, random edges to place special characters.
+			if ($howManySpecialCharacters > 0)
+			{
+				$locationOfSpecialCharacters	= [];
+				$t	= $howManySpecialCharacters;
+
+				for ($i = 0; $i < ($edges - $t); $i++) $locationOfSpecialCharacters[] = 0;
+				for ($i = 0; $i < $t; $i++) $locationOfSpecialCharacters[] = 1;
+
+				shuffle($locationOfSpecialCharacters);
+			}
 
 			// Boom!
 			for ($i = 0; $i < $edges; $i++)
@@ -39,7 +55,18 @@ final class PasswordGenerator
 						$howManyNumbers--;
 					}
 				}
-				
+
+				if ($howManySpecialCharacters > 0)
+				{
+					if ($locationOfSpecialCharacters[$i] == 1)
+					{
+						// TODO: This is sort of dirty, there's probably a more diomatic way of doing this.
+						$password	.= $specialCharacters[0];
+						$specialCharacters = substr($specialCharacters, 1, $howManySpecialCharacters);
+						$howManySpecialCharacters--;
+					}
+				}
+
 				if ($howManyWords > 0)
 				{
 					$password	.= $parser->returnWord($this->randNum(0, $a-1));
